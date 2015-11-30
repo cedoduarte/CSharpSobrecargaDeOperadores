21772662b8e0e94ce9e3eda48721f5aa75bd2979
cedoduarte <carlosduarte.1@hotmail.com>
2015-11-29 20:23:00 -0600

    first commit

---
+++ SobrecargaDeOperadores/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaDeOperadores
+{
+    public class Persona
+    {
+        public Persona(double salario)
+        {
+            Salario = salario;
+        }
+
+        public static double operator +(Persona x, Persona y)
+        {
+            return x.Salario + y.Salario;
+        }
+
+        public double Salario { get; set; }
+    }
+}
---
+++ SobrecargaDeOperadores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaDeOperadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var juan = new Persona(5000.0);
+            var ana = new Persona(6000.0);
+
+            Console.WriteLine(juan + ana);
+
+            Console.Read();
+        }
+    }
+}
